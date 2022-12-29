2530fec13e6380d77e3b612f113a3b91c91417ee
yoonstar1996 <yagobo1110@naver.com>
2022-12-29 19:02:36 +0900

    2022-12-29-19:02 Part1.ch10.12 finish

--- fastcampus/FrontEnd-start/Part1/starbucks/js/main.js
+++ fastcampus/FrontEnd-start/Part1/starbucks/js/main.js
@@ -13,3 +13,24 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
