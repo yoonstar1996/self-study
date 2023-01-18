7c4540b78306eb31a8bdcbdd00cbdd4721f2a62e
yoonstar1996 <yagobo1110@naver.com>
2023-01-18 17:02:13 +0900

    Part10.Ch5.03.finish

--- fastcampus/FrontEnd-start/Part10/style-loaders-example/src/App.js
+++ fastcampus/FrontEnd-start/Part10/style-loaders-example/src/App.js
@@ -1,23 +1,20 @@
 import logo from "./logo.svg";
 // import "./App.css";
-import "./App.scss";
+// import "./App.scss";
+import styles from "./App.module.css";
+import Button from "./components/Button";
+
+console.log(styles);
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <img src={logo} className="logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>버튼</Button>
       </header>
     </div>
   );
