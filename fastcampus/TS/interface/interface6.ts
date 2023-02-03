95154afbdeb540411d612f660ced2cee415a9abb
yoonstar1996 <yagobo1110@naver.com>
2023-02-03 18:21:31 +0900

    Part6.Ch5

---
+++ fastcampus/TS/interface/interface6.ts
@@ -0,0 +1,14 @@
+interface IPerson2 {
+  name: string;
+  age?: number;
+}
+
+interface IKorean extends IPerson2 {
+  city: string;
+}
+
+const k: IKorean = {
+  name: "Mark",
+  city: "Seoul",
+  age: 39,
+};
