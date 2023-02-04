948c78eccfac44ffda46fdc7f50a42378d3f6e64
yoonstar1996 <yagobo1110@naver.com>
2023-02-04 20:40:23 +0900

    Part6.Ch6.08

--- fastcampus/TS/class/example.ts
+++ fastcampus/TS/class/example.ts
@@ -1,19 +1,17 @@
 class Person {
-  public constructor(public _name: string, public age?: number) {}
+  public readonly name: string = "Mark";
+  private readonly country: string = "Korea";
 
-  get name() {
-    //
-    // console.log("get");
-    return this._name + "Lee";
+  public constructor(private _name: string, private age?: number) {
+    this.country = "U.S.A";
   }
 
-  set name(n: string) {
-    console.log("set");
-    this._name = n;
+  hello() {
+    // this.country = "U.S.A";
   }
 }
 
 const p1 = new Person("Mark", 39);
 console.log(p1.name); // get을 하는 함수 getter
-p1.name = "Yoon"; // set을 하는 함수 setter
+// p1.name = "Yoon"; // set을 하는 함수 setter
 console.log(p1.name); //
