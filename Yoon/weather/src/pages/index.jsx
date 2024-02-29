f9bc2f04e8577ba7d5f4b1cfd6be05a1d88d8e8a
yoonstar1996 <yagobo1110@naver.com>
2024-02-29 21:23:19 +0900

    scss

---
+++ Yoon/weather/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.wrap}>
+        <div className={styles.container}></div>
+      </div>
+    </>
+  );
+}
