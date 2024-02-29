d8beff48380af9a7d6a041c065b01127a126767f
yoonstar1996 <yagobo1110@naver.com>
2024-02-29 22:47:19 +0900

    weatherBox.scss

---
+++ Yoon/weather/src/components/MainView.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import styles from "./MainView.module.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faSearchLocation,
+  faChartLine,
+  faCog,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function MainView() {
+  return (
+    <>
+      <div className={styles.leftContainer}>
+        <div id={styles.cityNameBox}>
+          <div className={styles.cityName}>
+            <p>San Fransisco</p>
+            <p>Jan 28</p>
+          </div>
+        </div>
+        <div id={styles.contentsBox}>
+          <div className={styles.buttonBox}>
+            <div className={styles.buttonBackground}>
+              <button className={styles.forecast}>forecast</button>
+              <button className={styles.airquality}>airquality</button>
+            </div>
+          </div>
+          <div className={styles.weatherBox}>
+            <div className={styles.weatherDegree}>
+              <p>10&deg;</p>
+            </div>
+            <div className={styles.weatherIcon}>
+              <img src="/images/10d.png" alt="MianLogo" />
+            </div>
+            <div className={styles.weahterData}>
+              <div>
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id={styles.todayWeather}>
+          <div className={styles.textBox}>
+            <p>시간대별 날씨 정보</p>
+            <p>이번주 날씨 보기</p>
+          </div>
+          <div className={styles.timelyWeatherBox}>
+            <div className={styles.timelyWeather}>
+              <div className={styles.icon}>
+                <img src="/images/09d.png" alt="29" />
+              </div>
+              <div className={styles.data}>
+                <p className={styles.time}>2pm</p>
+                <p className={styles.currentDegree}>32&deg;</p>
+                <div>
+                  <img src="/images/drop.png" alt="drop" />
+                  <p>15%</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <nav>
+          <FontAwesomeIcon icon={faHome} />
+          <FontAwesomeIcon icon={faSearchLocation} />
+          <FontAwesomeIcon icon={faChartLine} />
+          <FontAwesomeIcon icon={faCog} />
+        </nav>
+      </div>
+    </>
+  );
+}
--- Yoon/weather/src/pages/index.jsx
+++ Yoon/weather/src/pages/index.jsx
@@ -1,10 +1,13 @@
+import MainView from "@/components/MainView";
 import styles from "./index.module.scss";
 
 export default function Home() {
   return (
     <>
       <div className={styles.wrap}>
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+          <MainView />
+        </div>
       </div>
     </>
   );
