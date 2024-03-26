02973a1333a1bea841e3f983f53b679d281306ef
yoonstar1996 <yagobo1110@naver.com>
2024-03-26 21:12:29 +0900

    java1-6.5

---
+++ opentutorials/java/Data_and_operation/StringApp.java
@@ -0,0 +1,14 @@
+public class StringApp {
+  public static void main(String[] args) {
+    
+    // Character vs String
+    System.out.println("Hello World"); // String
+    System.out.println('H'); // Character
+
+    // new line
+    System.out.println("Hello\nWorld");
+
+    // esscape
+    System.out.println("Hello \"World\"");
+  }
+}
