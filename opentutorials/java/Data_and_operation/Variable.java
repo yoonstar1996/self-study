6902e90a30774316a21b99e87a83c7dbab27ecbf
yoonstar1996 <yagobo1110@naver.com>
2024-03-27 18:13:43 +0900

    java1-11.2

---
+++ opentutorials/java/Data_and_operation/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+  public static void main(String[] args) {
+    
+    int a = 1; // Number -> integer
+    System.out.println(a);
+
+    double b = 1.1; // real number
+    System.out.println(b);
+
+    String c = "Hello world";
+    System.out.println(c);
+    
+  }
+}
