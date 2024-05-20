b730332156cd5ab06aa1614592ef5ec2b1f1b707
yoonstar1996 <yagobo1110@naver.com>
2024-05-20 18:16:46 +0900

    project.start

---
+++ Yoon/homepage/src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+export default function Home() {
+  return (
+    <>
+      <div>하이</div>
+      <div>ㅎ아</div>
+    </>
+  );
+}
