4e821c652506e0625c69a6c0ef9c903da645f0c0
yoonstar1996 <yagobo1110@naver.com>
2024-06-03 19:54:05 +0900

    musign.finish

--- Yoon/musign/js/script_main.js
+++ Yoon/musign/js/script_main.js
@@ -9,3 +9,46 @@ $(function () {
     $("span.bar").css({ left: 0, width: 0, opacity: 0 });
   });
 });
+
+// circleBox SVG길이 구하기
+$(function () {
+  $(".svgAni")
+    .find("path")
+    .each(function (i, path) {
+      let length = path.getTotalLength();
+    });
+});
+
+// 스크롤 애니메이션
+$(function () {
+  $(".animate").scrolla({
+    mobile: true, // 모바일 버전시 활성화
+    once: true, // 스크롤시 딱 한번만 하고 싶을 땐 true
+  });
+});
+
+// 배경색 변경
+$(window).on("scroll resize", function () {
+  let scrollTop = $(document).scrollTop();
+  bgColor();
+  function bgColor() {
+    if (scrollTop > 1400) {
+      $("body").addClass("on");
+    } else {
+      $("body").removeClass("on");
+    }
+    if (scrollTop > 2700) {
+      $("body").removeClass("on");
+    }
+  }
+});
+
+// 햄버거 메뉴
+$(function () {
+  $(".menuOpen button.open").on("click", function () {
+    $(".menuOpen .menuWrap").addClass("on");
+  });
+  $(".menuOpen .menuWrap .close").on("click", function () {
+    $(".menuOpen .menuWrap").removeClass("on");
+  });
+});
