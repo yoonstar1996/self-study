d1738c8040de07330407a0bc646631813ee14cac
yoonstar1996 <yagobo1110@naver.com>
2024-06-10 18:39:18 +0900

    nextjs.food.project.start

--- udemy/next14-project/app/about/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AboutPage() {
-  return (
-    <main>
-      <h1>AboutPage</h1>
-    </main>
-  );
-}
--- udemy/next14-project/app/page.js
+++ udemy/next14-project/app/page.js
@@ -1,15 +1,9 @@
-import Link from "next/link";
-import Header from "@/components/header";
-
 export default function Home() {
-  console.log("실행중");
   return (
     <main>
-      <Header />
-      <p>🔥 Lets get started! 🔥</p>
-      <p>
-        <Link href="/about">About us</Link>
-      </p>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
