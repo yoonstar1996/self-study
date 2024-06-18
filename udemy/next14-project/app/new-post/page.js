066adcd3969f4024f34480732d888eaca457ec50
yoonstar1996 <yagobo1110@naver.com>
2024-06-18 20:05:47 +0900

    nextjs.section6.172.finish

--- udemy/next14-project/app/new-post/page.js
+++ udemy/next14-project/app/new-post/page.js
@@ -1,46 +1,41 @@
+import PostForm from "@/components/postForm";
 import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     "use server";
     const title = formData.get("title");
     const image = formData.get("image");
     const content = formData.get("content");
 
-    storePost({
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required");
+    }
+
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required");
+    }
+
+    if (!image) {
+      errors.push("Image is required");
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
+    await storePost({
       imageUrl: "",
       title,
       content,
       userId: 1,
     });
+
+    redirect("/feed");
   }
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
---
+++ udemy/next14-project/components/formSubmit.js
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+import { useFormStatus } from "react-dom";
+
+export default function FormSubmit() {
+  const status = useFormStatus();
+
+  if (status.pending) {
+    return <p>Creating post...</p>;
+  }
+
+  return (
+    <>
+      <button type="reset">Reset</button>
+      <button>Create Post</button>
+    </>
+  );
+}
