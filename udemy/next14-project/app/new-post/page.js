6047d1898f73000971319a74d1da68f752feaa4e
yoonstar1996 <yagobo1110@naver.com>
2024-06-19 18:02:38 +0900

    nextjs.section6.173.finish

--- udemy/next14-project/app/new-post/page.js
+++ udemy/next14-project/app/new-post/page.js
@@ -1,41 +1,6 @@
+import { createPost } from "@/actions/posts";
 import PostForm from "@/components/postForm";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
-  async function createPost(prevState, formData) {
-    "use server";
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
-
-    let errors = [];
-
-    if (!title || title.trim().length === 0) {
-      errors.push("Title is required");
-    }
-
-    if (!content || content.trim().length === 0) {
-      errors.push("Content is required");
-    }
-
-    if (!image) {
-      errors.push("Image is required");
-    }
-
-    if (errors.length > 0) {
-      return { errors };
-    }
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed");
-  }
-
   return <PostForm action={createPost} />;
 }
