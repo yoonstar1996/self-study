a47d8a25aabd9b66e9496b59a7c5de38f3e94be3
yoonstar1996 <yagobo1110@naver.com>
2024-06-21 14:32:56 +0900

    change.projectName

---
+++ udemy/next14-posts_project/app/new-post/page.js
@@ -0,0 +1,6 @@
+import { createPost } from "@/actions/posts";
+import PostForm from "@/components/postForm";
+
+export default function NewPostPage() {
+  return <PostForm action={createPost} />;
+}
