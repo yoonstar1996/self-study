0abc502498c703b00b65638509179ccbd637dfdf
yoonstar1996 <yagobo1110@naver.com>
2024-06-24 17:54:18 +0900

    nextjs.section8.finish

--- udemy/next14-posts_project/app/feed/page.js
+++ udemy/next14-posts_project/app/feed/page.js
@@ -1,5 +1,19 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// export const metadata = {
+//   title: "Browse all our X posts.",
+//   description: "Browse all our posts.",
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all our ${numberOfPosts} posts.`,
+    description: "Browse all our posts.",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
--- udemy/next14-posts_project/components/header.js
+++ udemy/next14-posts_project/components/header.js
@@ -1,11 +1,19 @@
-import logo from '@/assets/logo.png';
-import Link from 'next/link';
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          alt="Mobile phone with posts feed on it"
+          width={100}
+          height={100}
+          priority
+          // sizes="10vw"
+        />
       </Link>
       <nav>
         <ul>
@@ -13,7 +21,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
