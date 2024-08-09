d38bc3262ec219dec455f8d33df4a4252510a3f0
yoonstar1996 <yagobo1110@naver.com>
2024-08-09 17:54:22 +0900

    tpyescript.section10.finish

---
+++ udemy/typescript/Maximilian/section9/src/components/project-items.ts
@@ -0,0 +1,49 @@
+import { Component } from "../components/base-components.js";
+import { Draggable } from "../models/drag-drop.js";
+import { Autobind } from "../decorators/autobind.js";
+import { Project } from "../models/project.js";
+
+// ProjectItem Class
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return `1 person`;
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  @Autobind
+  dragStartHandler(e: DragEvent): void {
+    e.dataTransfer!.setData("text/plain", this.project.id);
+    e.dataTransfer!.effectAllowed = "move";
+  }
+
+  dragEndHandler(_: DragEvent): void {
+    console.log("Drag End");
+  }
+
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
+
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
