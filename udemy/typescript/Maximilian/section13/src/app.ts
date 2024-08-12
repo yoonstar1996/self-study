aeed0b27665b1c6eae44d5c628a84a7f8405389b
yoonstar1996 <yagobo1110@naver.com>
2024-08-12 15:47:09 +0900

    typescript.section13.finish

---
+++ udemy/typescript/Maximilian/section13/src/app.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyDOERlIg6fLQJE3RSB7nDpi-85OxiG9Pl4";
+
+declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(e: Event) {
+  e.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // send this to Google's API
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
