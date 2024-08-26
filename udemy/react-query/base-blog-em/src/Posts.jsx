0d811f83fa05423641cc16d1ce4299655b35923e
yoonstar1996 <yagobo1110@naver.com>
2024-08-26 17:31:11 +0900

    react-query.section1.finish

---
+++ udemy/react-query/base-blog-em/src/Posts.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import { fetchPosts, deletePost, updatePost } from "./api";
+import { PostDetail } from "./PostDetail";
+const maxPostPage = 10;
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+    staleTime: 2000, // 2s
+  });
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
