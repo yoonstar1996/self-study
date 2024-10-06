eb6402c7b1000d3a6501aeb54afea3f7a9981bbc
yoonstar1996 <yagobo1110@naver.com>
2024-10-06 17:48:11 +0900

    onebite-next.section02.8 finish

--- udemy/nextjs/onebite-next/section02/src/pages/search/index.tsx
+++ udemy/nextjs/onebite-next/section02/src/pages/search/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { useRouter } from "next/router";
+import SearchableLayout from "@/components/searchable-layout";
 
 export default function Page() {
   const router = useRouter();
@@ -7,3 +8,7 @@ export default function Page() {
 
   return <h1>Search {q}</h1>;
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
