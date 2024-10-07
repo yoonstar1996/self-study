786b5c51866fd20d26e47e968813fa58a1ad4364
yoonstar1996 <yagobo1110@naver.com>
2024-10-07 17:19:53 +0900

    onebite-next.section02.11 finish

--- udemy/nextjs/onebite-next/section02/src/pages/index.tsx
+++ udemy/nextjs/onebite-next/section02/src/pages/index.tsx
@@ -1,10 +1,29 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.scss";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = () => {
+  const data = "hello";
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log("data: ", data);
+
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
