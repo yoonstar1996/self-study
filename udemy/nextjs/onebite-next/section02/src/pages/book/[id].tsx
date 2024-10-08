0db74c9ed1ae91753665064e666941703a3871b8
yoonstar1996 <yagobo1110@naver.com>
2024-10-08 16:20:02 +0900

    onebite-next.section02.15 finish

--- udemy/nextjs/onebite-next/section02/src/pages/book/[id].tsx
+++ udemy/nextjs/onebite-next/section02/src/pages/book/[id].tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import style from "./[id].module.scss";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import fetchOneBook from "@/lib/fetch-one-book";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
   return {
@@ -15,7 +24,7 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
