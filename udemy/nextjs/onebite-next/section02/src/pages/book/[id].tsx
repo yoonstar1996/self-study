30b1eeb9246c866253fdf507ca3a8ac36f7ef77c
yoonstar1996 <yagobo1110@naver.com>
2024-10-09 14:50:00 +0900

    onebite-next.section02.19 finish

--- udemy/nextjs/onebite-next/section02/src/pages/book/[id].tsx
+++ udemy/nextjs/onebite-next/section02/src/pages/book/[id].tsx
@@ -3,6 +3,7 @@ import style from "./[id].module.scss";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import fetchOneBook from "@/lib/fetch-one-book";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export const getStaticPaths = () => {
   return {
@@ -12,6 +13,9 @@ export const getStaticPaths = () => {
       { params: { id: "3" } },
     ],
     fallback: true,
+    // false : 404 NotFound
+    // blocking : SSR 방식
+    // true : SSR 방식 + 데이터가 없는 폴백 상태의 페이지
   };
 };
 
@@ -35,26 +39,47 @@ export default function Page({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  if (router.isFallback) return "로딩 중입니다.";
+  if (router.isFallback) {
+    return (
+      <>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서를 만나보세요."
+        />
+        <div>로딩중입니다.</div>
+      </>
+    );
+  }
 
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
     book;
   return (
-    <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
-      >
-        <img src={coverImgUrl} />
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={coverImgUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.cover_img_container}
+          style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        >
+          <img src={coverImgUrl} />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
       </div>
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>
-        {author} | {publisher}
-      </div>
-      <div className={style.description}>{description}</div>
-    </div>
+    </>
   );
 }
--- udemy/nextjs/onebite-next/section02/src/pages/index.tsx
+++ udemy/nextjs/onebite-next/section02/src/pages/index.tsx
@@ -5,6 +5,7 @@ import BookItem from "@/components/book-item";
 import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   const [allBooks, recommendBooks] = await Promise.all([
@@ -22,20 +23,31 @@ export default function Home({
   recommendBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>{`지금 추천하는 도서`}</h3>
-        {recommendBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>{`등록된 모든 도서`}</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서를 만나보세요."
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>{`지금 추천하는 도서`}</h3>
+          {recommendBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>{`등록된 모든 도서`}</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
