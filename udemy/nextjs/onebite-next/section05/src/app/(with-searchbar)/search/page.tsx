0c8a2da9d62cff539aa3f6d7bae7caf93d945a1b
yoonstar1996 <yagobo1110@naver.com>
2024-10-15 17:39:27 +0900

    onebite-next.section05.3 finish

--- udemy/nextjs/onebite-next/section05/src/app/(with-searchbar)/search/page.tsx
+++ udemy/nextjs/onebite-next/section05/src/app/(with-searchbar)/search/page.tsx
@@ -9,7 +9,8 @@ export default async function Page({
   };
 }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    { cache: "force-cache" }
   );
 
   if (!response.ok) {
--- udemy/nextjs/onebite-next/section05/src/app/book/[id]/page.tsx
+++ udemy/nextjs/onebite-next/section05/src/app/book/[id]/page.tsx
@@ -1,4 +1,11 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.scss";
+
+// export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 export default async function Page({
   params,
@@ -10,6 +17,9 @@ export default async function Page({
   );
 
   if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
     return <div>오류가 발생했습니다...</div>;
   }
 
