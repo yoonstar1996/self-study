8622f0eeafbeaa44c29dd57930b5d260ec9c2816
yoonstar1996 <yagobo1110@naver.com>
2024-10-15 17:04:58 +0900

    onebite-next.section05.2 finish

---
+++ udemy/nextjs/onebite-next/section05/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,60 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.scss";
+import { BookData } from "@/types";
+
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "force-cache" }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function RecommendBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const recommendBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {recommendBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+export default async function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecommendBooks />
+      </section>
+
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <AllBooks />
+      </section>
+    </div>
+  );
+}
