b74ac85596426c65b9dae89d61a372419c9e71e4
yoonstar1996 <yagobo1110@naver.com>
2024-10-21 17:25:06 +0900

    python beginner day1 finish

---
+++ udemy/python/Beginner/day1/task.py
@@ -0,0 +1,17 @@
+# print("Hello world!\nHello world!\nHello world!")
+
+# print("Hello" + "yoonstar")
+
+# input("이름을 입력해주세요. ")
+
+# print("Hello " + input("이름을 입력해주세요. ") + "!")
+
+# name = input("이름을 입력해주세요. ")
+# length = len(name)
+# print(name)
+# print(length)
+
+print("밴드 이름 생성기에 오신걸 환영합니다.")
+city = input("당신이 자라온 도시의 이름은 무엇인가요?\n")
+pet = input("당신의 반려동물의 이름은 무엇인가요?\n")
+print("당신의 밴드 이름은 " + city + " " + pet + "입니다.")
