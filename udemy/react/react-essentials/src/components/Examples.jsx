a3ada48858048d5b3eeb803026631ca5ec554178
yoonstar1996 <yagobo1110@naver.com>
2024-10-23 15:01:32 +0900

    section4.68.finish

--- udemy/react/react-essentials/src/components/Examples.jsx
+++ udemy/react/react-essentials/src/components/Examples.jsx
@@ -28,7 +28,6 @@ export default function Examples() {
   return (
     <Section id="examples" title="Examples">
       <Tabs
-        ButtonsContainer={"menu"}
         buttons={
           <>
             <TabButton
--- udemy/react/react-essentials/src/components/Tabs.jsx
+++ udemy/react/react-essentials/src/components/Tabs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Tabs({ children, buttons, ButtonsContainer }) {
+export default function Tabs({ children, buttons, ButtonsContainer = "menu" }) {
   return (
     <>
       <ButtonsContainer>{buttons}</ButtonsContainer>
