cb129a6015ec14001966f50eeee728cbe15d3cc3
yoonstar1996 <yagobo1110@naver.com>
2024-10-30 18:31:48 +0900

    section9.147.finish

---
+++ lecture/react/max/project-task/src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+  return (
+    <main>
+      <ProjectSidebar />
+    </main>
+  );
+}
+
+export default App;
---
+++ lecture/react/max/project-task/src/components/ProjectSidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ProjectSidebar() {
+  return (
+    <aside>
+      <h2>Your Project</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
