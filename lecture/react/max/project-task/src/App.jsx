70d55c7bd1feb8c85aa22ccd33f980662d3f9fc8
yoonstar1996 <yagobo1110@naver.com>
2024-10-31 13:39:31 +0900

    section8.152.finish

--- lecture/react/max/project-task/src/App.jsx
+++ lecture/react/max/project-task/src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- lecture/react/max/project-task/src/components/NoProjectSelected.jsx
+++ lecture/react/max/project-task/src/components/NoProjectSelected.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -17,7 +17,7 @@ export default function NoProjectSelected() {
         Select a project or get started with a new one
       </p>
       <p className="mt-8">
-        <Button>Create new Project</Button>
+        <Button onClick={onStartAddProject}>Create new Project</Button>
       </p>
     </div>
   );
