a47f5b8ed26d4c004cfc617f84b9a4170abd7e3b
yoonstar1996 <yagobo1110@naver.com>
2024-10-31 12:54:14 +0900

    section8.149.finish

---
+++ lecture/react/max/project-task/src/components/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Input({ label, textarea, ...props }) {
+  return (
+    <p>
+      <label>{label}</label>
+      {textarea ? <textarea {...props} /> : <input {...props} />}
+    </p>
+  );
+}
