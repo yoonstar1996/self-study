91636d605992ae1570630aacf74ff395bc4d2d99
yoonstar1996 <yagobo1110@naver.com>
2024-11-01 20:32:22 +0900

    section10;167.finish

--- lecture/react/max/react-context/src/App.jsx
+++ lecture/react/max/react-context/src/App.jsx
@@ -70,7 +70,7 @@ function App() {
   }
 
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={{ items: [] }}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
