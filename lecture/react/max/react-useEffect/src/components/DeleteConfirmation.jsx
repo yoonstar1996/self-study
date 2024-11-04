2a1e2eed6b6e0fa7ab8301fc6be087f9a5b0908b
yoonstar1996 <yagobo1110@naver.com>
2024-11-04 14:05:50 +0900

    section11.finish

--- lecture/react/max/react-useEffect/src/components/DeleteConfirmation.jsx
+++ lecture/react/max/react-useEffect/src/components/DeleteConfirmation.jsx
@@ -1,20 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
 
 const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRemainingTime((prevTime) => prevTime - 10);
-    }, 10);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       console.log("Timer set");
@@ -39,7 +28,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
