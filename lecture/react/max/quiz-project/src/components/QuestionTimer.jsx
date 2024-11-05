90a3348ee1a63beb702ae3e83b638714d71fe695
yoonstar1996 <yagobo1110@naver.com>
2024-11-05 21:04:59 +0900

    section12.finish

--- lecture/react/max/quiz-project/src/components/QuestionTimer.jsx
+++ lecture/react/max/quiz-project/src/components/QuestionTimer.jsx
@@ -4,7 +4,6 @@ export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    console.log("SETTING TIMEOUT");
     const timer = setTimeout(onTimeout, timeout);
 
     return () => {
@@ -13,7 +12,6 @@ export default function QuestionTimer({ timeout, onTimeout, mode }) {
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    console.log("SETTING INTERVAL");
     const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
--- lecture/react/max/quiz-project/src/components/Quiz.jsx
+++ lecture/react/max/quiz-project/src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
-import QuizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -22,12 +22,7 @@ export default function Quiz() {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={QuizCompleteImg} alt="Quiz Complete" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
