1143556e9f97780fffe27118c446792a4f3589e0
yoonstar1996 <yagobo1110@naver.com>
2024-11-07 15:37:34 +0900

    section15.236.finish

---
+++ lecture/react/max/react-http/src/http.js
@@ -0,0 +1,10 @@
+export const fetchAvailablePlaces = async () => {
+  const response = await fetch("http://localhost:3000/places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch places");
+  }
+
+  return resData.places;
+};
