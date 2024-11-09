bdec8deb256023351713bc51cb8cba5f57fb5425
yoonstar1996 <yagobo1110@naver.com>
2024-11-09 14:36:10 +0900

    section18.268.finish

--- lecture/react/max/food-order-app/src/components/Meals.jsx
+++ lecture/react/max/food-order-app/src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import MealItem from "./MealItem";
 
 export default function Meals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
@@ -22,7 +23,7 @@ export default function Meals() {
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-        <li key={meal.id}>{meal.name}</li>
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
