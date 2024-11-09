ff614af6e4213484e9d2b97c3e5d87692f387148
yoonstar1996 <yagobo1110@naver.com>
2024-11-09 15:16:33 +0900

    section18.270.finish

---
+++ lecture/react/max/food-order-app/src/store/CartContext.jsx
@@ -0,0 +1,48 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingCartItem = state.items[existingCartItemIndex];
+
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    //
+  }
+
+  return state;
+};
+
+export const CartContextProvider = ({ children }) => {
+  useReducer(cartReducer, {
+    items: [],
+  });
+
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+};
+
+export default CartContext;
