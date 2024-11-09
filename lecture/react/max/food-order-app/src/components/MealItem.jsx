a1f9f2e7d86f6de07885a6c4b1bf603e8d36ed0d
yoonstar1996 <yagobo1110@naver.com>
2024-11-09 15:00:43 +0900

    section18.269.finish

--- lecture/react/max/food-order-app/src/components/MealItem.jsx
+++ lecture/react/max/food-order-app/src/components/MealItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
 
 export default function MealItem({ meal }) {
   return (
@@ -14,7 +15,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
---
+++ lecture/react/max/food-order-app/src/components/UI/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Button({ children, textOnly, className, ...props }) {
+  const cssClasses = textOnly
+    ? `text-button ${className}`
+    : `button ${className}`;
+
+  return (
+    <button className={cssClasses} {...props}>
+      {children}
+    </button>
+  );
+}
