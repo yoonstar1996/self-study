d5e66847f799c8024c431f8a3c56bedada396365
Yoonstar <yagobo1110@naver.com>
2025-07-04 15:31:47 +0900

    Code: 프로그래머스 lv1 - 콜라츠 추측

---
+++ programmers/lv_1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+  var answer = 0;
+  if (num === 1) return 0;
+
+  while (answer <= 500) {
+    if (num === 1) return answer;
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num = num * 3 + 1;
+    }
+    answer++;
+    if (answer > 500) return -1;
+  }
+
+  return answer;
+}
