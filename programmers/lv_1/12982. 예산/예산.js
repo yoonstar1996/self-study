828fb09a980a2a170cc15c51f717716fab077d5c
Yoonstar <yagobo1110@naver.com>
2025-07-11 10:25:00 +0900

    Code: 프로그래머스 lv1 - 예산

---
+++ programmers/lv_1/12982. 예산/예산.js
@@ -0,0 +1,15 @@
+function solution(d, budget) {
+  var answer = 0;
+  d.sort((a, b) => a - b);
+
+  for (let i = 0; i < d.length; i++) {
+    if (budget >= d[i]) {
+      budget -= d[i];
+      answer++;
+    } else {
+      break;
+    }
+  }
+
+  return answer;
+}
