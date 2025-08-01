df43bfcc63a0f1695182d3699c25507ef9c25823
Yoonstar <yagobo1110@naver.com>
2025-08-01 20:34:01 +0900

    Feat: 댓글 react-query 적용

--- udemy/react-query/base-blog-em/src/PostDetail.jsx
+++ udemy/react-query/base-blog-em/src/PostDetail.jsx
@@ -1,9 +1,19 @@
-import { fetchComments } from "./api";
 import "./PostDetail.css";
 
+import Error from "./Error";
+import Loading from "./Loading";
+import { fetchComments } from "./api";
+import { useQuery } from "@tanstack/react-query";
+
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) return <Loading />;
+
+  if (isError) return <Error error={error} />;
 
   return (
     <>
