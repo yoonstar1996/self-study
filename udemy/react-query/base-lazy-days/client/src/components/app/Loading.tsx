fd71c2fc72df8b824b055f5cb5790582edc9e287
Yoonstar <yagobo1110@naver.com>
2025-08-05 18:42:28 +0900

    Feat: Loading 컴포넌트에 useIsFetching 적용

--- udemy/react-query/base-lazy-days/client/src/components/app/Loading.tsx
+++ udemy/react-query/base-lazy-days/client/src/components/app/Loading.tsx
@@ -1,8 +1,8 @@
 import { Spinner, Text } from "@chakra-ui/react";
+import { useIsFetching } from "@tanstack/react-query";
 
 export function Loading() {
-  // will use React Query `useIsFetching` to determine whether or not to display
-  const isFetching = false; // for now, just don't display
+  const isFetching = useIsFetching();
   const display = isFetching ? "inherit" : "none";
 
   return (
