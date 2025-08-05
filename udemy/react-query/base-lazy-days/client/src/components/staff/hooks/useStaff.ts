3e05ca9b2ab8086968ffd2a25b3424efdf5ee024
Yoonstar <yagobo1110@naver.com>
2025-08-05 19:14:49 +0900

    Feat: useStaff훅 변경

--- udemy/react-query/base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ udemy/react-query/base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,24 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 import type { Staff } from "@shared/types";
 
-import { filterByTreatment } from "../utils";
-
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
-// query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get("/staff");
+  return data;
+}
 
 export function useStaff() {
-  // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
+  const fallback: Staff[] = [];
 
-  // TODO: get data from server via useQuery
-  const staff: Staff[] = [];
+  const { data: staff = fallback } = useQuery({
+    queryKey: [queryKeys.staff],
+    queryFn: getStaff,
+  });
 
   return { staff, filter, setFilter };
 }
--- udemy/react-query/base-lazy-days/client/src/react-query/queryClient.ts
+++ udemy/react-query/base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,19 +1,23 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
 
-// function errorHandler(errorMsg: string) {
-//   // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
-//   // one message per page load, not one message per query
-//   // the user doesn't care that there were three failed queries on the staff page
-//   //    (staff, treatments, user)
-//   const id = "react-query-toast";
+import { toast } from "@/components/app/toast";
 
-//   if (!toast.isActive(id)) {
-//     const action = "fetch";
-//     const title = `could not ${action} data: ${
-//       errorMsg ?? "error connecting to server"
-//     }`;
-//     toast({ id, title, status: "error", variant: "subtle", isClosable: true });
-//   }
-// }
+function errorHandler(errorMsg: string) {
+  const id = "react-query-toast";
 
-export const queryClient = new QueryClient();
+  if (!toast.isActive(id)) {
+    const action = "fetch";
+    const title = `could not ${action} data: ${
+      errorMsg ?? "error connecting to server"
+    }`;
+    toast({ id, title, status: "error", variant: "subtle", isClosable: true });
+  }
+}
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      errorHandler(error.message);
+    },
+  }),
+});
