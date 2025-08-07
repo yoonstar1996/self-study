9ab06c0f8c3e6873714955aff2fc1e7921f59558
Yoonstar <yagobo1110@naver.com>
2025-08-07 19:16:38 +0900

    Feat: appointment select 함수 생성

--- udemy/react-query/base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ udemy/react-query/base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,8 +1,9 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import dayjs from "dayjs";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { AppointmentDateMap } from "../types";
+import { getAvailableAppointments } from "../utils";
 import { getMonthYearDetails, getNewMonthYear } from "./monthYear";
 
 import { useLoginData } from "@/auth/AuthContext";
@@ -50,6 +51,15 @@ export function useAppointments() {
   //   appointments that the logged-in user has reserved (in white)
   const { userId } = useLoginData();
 
+  const selectFn = useCallback(
+    (data: AppointmentDateMap, showAll: boolean) => {
+      if (showAll) return data;
+
+      return getAvailableAppointments(data, userId);
+    },
+    [userId]
+  );
+
   /** ****************** END 2: filter appointments  ******************** */
   /** ****************** START 3: useQuery  ***************************** */
 
@@ -72,6 +82,7 @@ export function useAppointments() {
   const { data: appointments = fallback } = useQuery({
     queryKey: [queryKeys.appointments, monthYear.year, monthYear.month],
     queryFn: () => getAppointments(monthYear.year, monthYear.month),
+    select: (data) => selectFn(data, showAll),
   });
 
   /** ****************** END 3: useQuery  ******************************* */
