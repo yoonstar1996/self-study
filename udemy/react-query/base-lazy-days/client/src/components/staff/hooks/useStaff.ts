51f1157b38dc3fbca508b6df9da0d5a2b5c9d417
Yoonstar <yagobo1110@naver.com>
2025-08-07 19:33:34 +0900

    Feat: useStaff select 함수 생성

--- udemy/react-query/base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ udemy/react-query/base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import type { Staff } from "@shared/types";
+
+import { filterByTreatment } from "../utils";
 
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
@@ -15,9 +17,19 @@ export function useStaff() {
   const [filter, setFilter] = useState("all");
   const fallback: Staff[] = [];
 
+  const selectFn = useCallback(
+    (data: Staff[], treatmentName: string) => {
+      if (treatmentName === "all") return data;
+
+      return filterByTreatment(data, filter);
+    },
+    [filter]
+  );
+
   const { data: staff = fallback } = useQuery({
     queryKey: [queryKeys.staff],
     queryFn: getStaff,
+    select: (data) => selectFn(data, filter),
   });
 
   return { staff, filter, setFilter };
