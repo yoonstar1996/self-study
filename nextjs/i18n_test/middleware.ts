b7ddbb7475fb0e8f77d71f89a4bc8f5dc303899c
Yoonstar <yagobo1110@naver.com>
2025-09-04 15:36:13 +0900

    Remove: i18n 테스트 삭제

--- nextjs/i18n_test/middleware.ts
+++
@@ -1,8 +0,0 @@
-import createMiddleware from "next-intl/middleware";
-import intlConfig from "./next-intl.config";
-
-export default createMiddleware(intlConfig);
-
-export const config = {
-    matcher: ["/", "/(ko|en)/:path*"],
-};
--- nextjs/i18n_test/next-intl.config.ts
+++
@@ -1,9 +0,0 @@
-export const locales = ["ko", "en"] as const;
-export const defaultLocale = "ko" as const;
-
-const config = {
-    locales: [...locales],
-    defaultLocale,
-};
-
-export default config;
